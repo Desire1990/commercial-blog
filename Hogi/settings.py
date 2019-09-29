b50425ad9714e4a1921ecbfefa1fc35409f7a8c1
Konstrictor <jonkur96@gmail.com>
2019-09-29 08:22:53 +0200

    download thumbnail pdf pour les utilisateurs qui n'ont pas payé

--- Hogi/settings.py
+++ Hogi/settings.py
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
@@ -126,6 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+LOGIN_URL = "/connexion/"
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
--- librairie/models.py
+++ librairie/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.utils.text import slugify
+from PyPDF2 import PdfFileReader, PdfFileWriter
+from django.core.files.base import ContentFile
+from django.core.files import File
+import os
 
 class Commentaire(models.Model):
     livre = models.ForeignKey('Livre', on_delete=models.CASCADE)
@@ -73,9 +77,35 @@ class Livre(models.Model):
     annee = models.DateField(default=timezone.now)
     contributeurs = models.ManyToManyField("base.Profil", through='Contribution')
     prix = models.IntegerField(verbose_name="prix")
-    livre = models.FileField(upload_to="librairie/livres")    
+    livre = models.FileField(upload_to="librairie/livres")
+    thumbnail = models.FileField(null=True, upload_to="librairie/livres")  
     version = models.IntegerField(verbose_name="version du livre")
     slug = models.SlugField(unique=True, max_length=30)
 
+    def save(self, *args, **kwargs):
+        self.thumbnail.name = "librairie/livres/"+self.thumbName()
+        super(Livre, self).save(*args, **kwargs)
+        print("===== thumb =====", self.thumbnail.url)
+        self.generateThumbPdf()
+
+    def generateThumbPdf(self):
+        thumb_dir = os.path.dirname(self.livre.path)
+        thumbName = os.path.basename(self.thumbnail.name)
+        thumbfile = os.path.join(thumb_dir, thumbName)
+        file = open(self.livre.path, 'rb')
+        output = PdfFileWriter()
+        pdfOne = PdfFileReader(file)
+        for i in range(10):
+            output.addPage(pdfOne.getPage(i))
+        pdf = open(thumbfile, 'ab+')
+        output.write(pdf)
+        pdf.close()
+
+    def thumbName(self):
+        thumb_name, thumb_extension = os.path.splitext(self.livre.name)
+        thumb_extension = thumb_extension.lower()
+        thumb_filename = thumb_name + '_thumb' + thumb_extension
+        return thumb_filename
+                
     def __str__(self):
         return f"{self.titre} {self.version} {self.annee}"
