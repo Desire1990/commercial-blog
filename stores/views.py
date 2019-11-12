978e2b707c43a504c069753f050c5439ffb8a376
Desos_Wajos <55079306+Desire1990@users.noreply.github.com>
2019-11-12 11:59:23 +0200

    Update views.py
    Adding pagination

--- stores/views.py
+++ stores/views.py
@@ -6,25 +6,25 @@ from django.contrib.auth.decorators import login_required
 from django.utils.datastructures import MultiValueDictKeyError
 from .forms import PostForm
 
-def article_attrs( post_obj, page ):
+def article_attrs( posts, page ):
     try:
-        slide1 = post_obj[0]
-        slides = post_obj[1:3]
+        slide1 = posts[0]
+        slides = posts[1:3]
     except IndexError:
         slide1 = None
         slides = None
-    pages        = Paginator( post_obj, 2 )
+    pages        = Paginator( posts, 2 )
     page_content = pages.page( page )
     pagination   = pages.page_range
     nom_app      = "stores"
     return (nom_app, slide1, slides, pages, page_content)
    
 def accueil( request ):
-    mdel = Post
+    model = Post
     nom_app    = "Articles"
     page_title = "Articles" 
     accueil    = True
-    post   = Post.objects.all().order_by('-created_on')
+    posts   = Post.objects.filter(status = 1).order_by('-created_on')
     try:
         page = int( request.GET["page"] )
     except MultiValueDictKeyError:
@@ -37,7 +37,7 @@ def accueil( request ):
 
 def post_view(request, pk, template_name='stores/postdetail.html'):
     post= get_object_or_404(Post, pk=pk) 
-    #post = Post.objects.all()   
+    #posts = Post.objects.all()   
     return render(request, template_name, {'post':post})
 
 
